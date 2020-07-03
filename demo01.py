be0bc03e1f922dcab8dfb7cda317b8bc87b287c5
cll355 <2236022218@qq.com>
2020-07-03 17:15:02 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,59 @@
+'''
+print("你好！")   # 字符串
+print(2333)  # 整数
+print(2.33) #小数
+print(True) #布尔值 true false
+print(())   #元组
+print([])   # 数组
+print({})   #字典
+
+ 多行注释
+ 还漏顶顶顶顶顶顶顶
+
+print("哈哈",23333,"aaa")  
+print("黑皮"+"坏坏")    # 字符串的拼接
+print("哈哈"*20)    # 倍数
+print(((1+2)*100-220)%2)
+print(2>3)
+print(3<4)
+
+
+变量
+赋值
+
+a = "张三"   #把张三的这个值复制给a这个变量   变量必须为小写字母
+print(a)
+
+
+a = input("请输入：")
+print("input获取的内容：",a)
+'''
+'''
+print(type("aaa"))
+print(type(111))
+print(type(1.11))
+print(type(True))
+print(type(()))
+print(type([]))
+print(type({}))
+
+将字符串转换为整型
+a = int("123")
+print(type(a))
+'''
+# a = float(input("第一个数："))
+# b = float(input("第二个数："))
+# print("两数之和：",a+b)
+
+# 字符串长度
+# a ='asadasdsfdfsdggdfgfd  '
+# b ='哈哈'
+# print(len(a))
+# print(len(b))
+
+# a = input("请输入一个数：")
+# b = input("请输入另一个数：")
+# print("两数相加之和：",a+b)
+# print(len(a+b))
+a='adsadasfasd'
+print(type(a))
